b9f3978b7cd677f103bf664a8b255154a131aecc
ManoelEric1212 <manoeleric59@gmail.com>
2023-05-28 15:29:35 -0300

    feat: Estruturando validação e transformação do formulário

--- formularios-react-hook-form/src/App.tsx
+++ formularios-react-hook-form/src/App.tsx
@@ -1,12 +1,31 @@
 import './styles/global.css'
 import './style.css'
-import { useForm } from 'react-hook-form';
+import { useForm, useFormState } from 'react-hook-form';
 import { ChangeEvent, useState } from 'react';
+import {z} from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+
+const createUserFormSchema = z.object({
+  name: z.string().nonempty('O nome é obrigatório').transform(name => {
+    return name.trim().split(' ').map(palavra =>{
+      return palavra[0].toLocaleUpperCase().concat(palavra.substring(1))
+    }).join(' ')
+  }),
+  email: z.string().nonempty('O e-mail é obrigatório').email('Formato de e-mail inválido'),
+  password: z.string().min(6, 'Senha precisa de no mínimo 6 caracteres')
+})
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>
+
+
+
 export default function App(){
-  const {register, handleSubmit} = useForm();
+  const {register, handleSubmit, formState: {errors}} = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  });
+  
   const [output, setOutput] = useState('');
   const [darkMode, setDarkMode] = useState(false);
-  console.log("saida", output)
 
   const handleToogleChange = (event: ChangeEvent<HTMLInputElement>)=>{
     setDarkMode(event.target.checked)
@@ -19,12 +38,23 @@ export default function App(){
     <main className={`h-screen ${darkMode ? 'bg-zinc-900 text-zinc-300': 'bg-zync-50'} flex flex-col items-center justify-center`}>
       <form onSubmit={handleSubmit(createUser)} className='flex flex-col gap-4 w-full max-w-xs'>
         <div className='flex flex-col gap-1'>
+          <label htmlFor=''>Nome</label>
+          <input
+          {...register('name')}
+           type='text'
+           className='border border-gray-400  shadow-sm rounded h-10 px-3'
+          />
+          {errors.name && (<span>{errors.name.message}</span>)}
+ 
+        </div>
+        <div className='flex flex-col gap-1'>
           <label htmlFor=''>E-mail</label>
           <input
           {...register('email')}
            type='email'
            className='border border-gray-400  shadow-sm rounded h-10 px-3'
           />
+          {errors.email && (<span>{errors.email.message}</span>)}
  
         </div>
         <div className='flex flex-col gap-1'>
@@ -35,6 +65,7 @@ export default function App(){
            className='border border-gray-400 shadow-sm rounded h-10 px-3'
           />
         </div>
+        {errors.password && (<span>{errors.password?.message}</span>)}
       
         <button
          type="submit"
